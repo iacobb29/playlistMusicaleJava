3ed72b538a5179e943aec20e8d16d6b3e1ad39dc
LI <danlorenzoiacob2020@olivelliputelli.it>
2024-04-24 10:42:59 +0200

    Inizio progetto

---
+++ src/main/java/com/mycompany/playlistmusicale/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlistmusicale;
+
+/**
+ *
+ * @author Studente
+ */
+public class App {
+
+    public static void main(String[] args) 
+    {
+       
+    }
+}

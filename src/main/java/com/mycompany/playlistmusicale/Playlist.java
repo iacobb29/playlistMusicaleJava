e1f7f1a29ea729830582f4ab60c2e042d2f42be8
iacob <iacob@DESKTOP-14EC4M9>
2024-05-06 18:44:38 +0200

    Aggiunto il menù con le funzioni cercaCanzone, rimuoviCanzone, elencoCanzoniTitolo, elencoCanzoniAutore, elencoCanzoniGenere, esports/importa su CSV e serializza/deserializza

--- src/main/java/com/mycompany/playlistmusicale/Playlist.java
+++ src/main/java/com/mycompany/playlistmusicale/Playlist.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,7 +24,7 @@ import utilita.TextFile;
  *
  * @author Studente
  */
-public class Playlist 
+public class Playlist implements Serializable
 {
     private final static int NUM_MAX_TRACCE = 10;
     private Canzone[] tracce;

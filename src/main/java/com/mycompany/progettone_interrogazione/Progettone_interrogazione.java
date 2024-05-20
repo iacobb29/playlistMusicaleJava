e79cb255011b548c669f05aa324f5f0c1121fa7e
LI <danlorenzoiacob2020@olivelliputelli.it>
2024-05-20 10:18:16 +0200

    .

---
+++ src/main/java/com/mycompany/progettone_interrogazione/Progettone_interrogazione.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progettone_interrogazione;
+
+/**
+ *
+ * @author Studente
+ */
+public class Progettone_interrogazione {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
